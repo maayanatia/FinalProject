ff4da0a066b498d44a8ace7d5348840ed49a8dcd
maayanatia <maayanatia1@gmail.com>
2026-06-18 13:17:12 +0300

    git push

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,6 @@ import { rateLimiter } from '../utils/rateLimiter.js';
 
 const router = express.Router();
 
-router.get('/advice/api', rateLimiter('advice', 1500), getRandomAdvice);
-router.get('/jokes/api', rateLimiter('joke', 1500), getRandomJoke);
 router.use(adviceRoutes);
 router.use(jokesRoutes);
 
